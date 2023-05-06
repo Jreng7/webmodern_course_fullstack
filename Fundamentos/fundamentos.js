6922a03623e207fbd19ac1f952a458011fb3cb34
Josue R S Jacinto <jr.waydev@gmail.com>
2023-05-05 22:18:03 -0300

    main no js

---
+++ Fundamentos/fundamentos.js
@@ -0,0 +1,1 @@
+console.log(' sentença...#1 ')  // Sentença de código.
--- fundamentos_1.0/inicio/primeiro.js
+++
@@ -1,5 +0,0 @@
-console.log('Bom dia...') // Uma sentença de código
-console.log('Boa tarde...') // 2ª sentença de código
-console.log('Boa noite...') // 3ª sentença de código
-
-// No total temos 3 sentenças de códigos acima.
